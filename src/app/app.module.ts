d8be97e8a7a6e8b0e364e0435437d27118b7f627
vatsal51 <vatsal539241@gmail.com>
2020-10-23 01:19:41 +0530

    created basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ThankyouComponent } from './thankyou/thankyou.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    LoginPageComponent,
+    PageNotFoundComponent,
+    ThankyouComponent
   ],
   imports: [
     BrowserModule
