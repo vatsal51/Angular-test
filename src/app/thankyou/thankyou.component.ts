a769fc037651df8e19bae30b9288f55524f528db
vatsal51 <vatsal539241@gmail.com>
2020-10-23 11:21:06 +0530

    created html pages and styling it

--- src/app/thankyou/thankyou.component.ts
+++ src/app/thankyou/thankyou.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-thankyou',
   templateUrl: './thankyou.component.html',
-  styleUrls: ['./thankyou.component.css']
+  styleUrls: ['./thankyou.component.scss']
 })
 export class ThankyouComponent implements OnInit {
 
